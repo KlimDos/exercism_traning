cbda4c65046da283a27280cbe5d0bb2df0624470
Sasha <klimdos@gmail.com>
2019-06-29 21:09:35 +0300

    add go project

---
+++ go/go/hello-world/helloworld.go
@@ -0,0 +1,16 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package greeting should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package main
+
+import (
+	"fmt"
+)
+
+// HelloWorld should have a comment documenting it.
+func main() {
+	fmt.Println("Hello, World!")
+	
+}
