600faf623e003fd233f8187eeb42c7c611222225
Sasha Alimov <aalimov@wiley.com.com>
2019-09-20 16:13:24 +0300

    demo 80% done

--- python/jupyter/Demo1/main.py
+++ python/jupyter/Demo1/main.py
@@ -1,7 +1,11 @@
 import nature.animals
 
+import simplemath
+
 
 d = nature.animals.Dog("Bob","27")
 print (d.introduce())
 d.age = nature.animals.pass_the_time(d.age,"10")
 print (d.introduce())
+
+print (simplemath.add("5","4"))
